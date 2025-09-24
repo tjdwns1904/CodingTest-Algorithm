08e166f63b6d5f01ef45c63e7ca5c87d12c5796d
tjdwns99 <beans2853@gmail.com>
2025-09-24 11:10:26 +0900

    [level 1] Title: 체육복, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,15 +1,11 @@
 function solution(n, lost, reserve) {
     var answer = 0;
-    const lostSet = new Set(lost.sort((a, b) => a - b));
-    const reserveSet = new Set(reserve.sort((a, b) => a - b));
+    const lostSet = new Set(lost.sort((a, b) => a - b).filter((a) => !reserve.includes(a)));
+    const reserveSet = new Set(reserve.sort((a, b) => a - b).filter((a) => !lost.includes(a)));
     for(const student of reserveSet){
-        if(lostSet.has(student) || lostSet.has(student - 1)){
-            const borrow = lostSet.has(student) ? student : student - 1;
-            reserveSet.delete(student);
+        if(lostSet.has(student + 1) || lostSet.has(student - 1)){
+            const borrow = lostSet.has(student - 1) ? student - 1 : student + 1;
             lostSet.delete(borrow);
-        }else if(lostSet.has(student + 1) && !reserveSet.has(student + 1)){
-            reserveSet.delete(student);
-            lostSet.delete(student + 1)
         }
     }
     answer = n - lostSet.size;
