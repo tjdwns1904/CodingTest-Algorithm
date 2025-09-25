f2d77c141c0216baec960e03884d4748c307d865
tjdwns99 <beans2853@gmail.com>
2025-09-25 16:18:40 +0900

    [Silver IV] Title: 설탕 배달, Time: 160 ms, Memory: 10772 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.js
@@ -0,0 +1,15 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let N = +input[0];
+let DP = Array(N + 1).fill(Infinity);
+const kgs = [3, 5];
+DP[0] = 0; 
+for(let i = 3; i <= N; i++){
+    for(const kg of kgs){
+        if(i - kg < 0 || DP[i - kg] === Infinity)continue;
+        DP[i] = Math.min(DP[i - kg] + 1, DP[i]);
+    }
+}
+
+console.log(DP[N] === Infinity ? -1 : DP[N]);
