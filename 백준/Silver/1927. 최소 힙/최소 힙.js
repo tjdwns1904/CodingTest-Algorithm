0ad5a39eaaa85672a9559859ef35a915a208b6ae
tjdwns99 <beans2853@gmail.com>
2025-10-01 15:25:39 +0900

    [Silver II] Title: 최소 힙, Time: 204 ms, Memory: 26140 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.js
@@ -0,0 +1,47 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let N = +input[0];
+const minHeap = [];
+const answer = [];
+
+for(let i = 1; i <= N; i++){
+    const c = +input[i];
+    if(c){
+        heapPush(minHeap, c);
+    }else{
+        answer.push(minHeap.length > 0 ? heapPop(minHeap) : 0);
+    }
+}
+
+console.log(answer.join('\n'));
+
+function heapPush(heap, item){
+    heap.push(item);
+    let idx = heap.length - 1;
+    while(idx > 0){
+        let parent = Math.floor((idx - 1) / 2);
+        if(heap[parent] <= heap[idx])break;
+        [heap[parent], heap[idx]] = [heap[idx], heap[parent]];
+        idx = parent;
+    }
+}
+
+function heapPop(heap){
+    if(heap.length === 0)return null;
+    if(heap.length === 1)return heap.pop();
+    const head = heap[0];
+    heap[0] = heap.pop();
+    let idx = 0;
+    let smallest = 0;
+    while(true){
+        let left = 2 * idx + 1;
+        let right = 2 * idx + 2;
+        if(left < heap.length && heap[left] < heap[smallest])smallest = left;
+        if(right < heap.length && heap[right] < heap[smallest])smallest = right;
+        if(smallest === idx)break;
+        [heap[idx], heap[smallest]] = [heap[smallest], heap[idx]];
+        idx = smallest;
+    }
+    return head;
+}
