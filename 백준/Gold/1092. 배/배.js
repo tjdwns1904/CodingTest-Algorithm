9aee8839b7baac4963a2aac522e10785715a4e28
tjdwns99 <beans2853@gmail.com>
2026-02-11 20:45:28 +0900

    [Gold V] Title: 배, Time: 164 ms, Memory: 10604 KB -BaekjoonHub

---
+++ 백준/Gold/1092. 배/배.js
@@ -0,0 +1,35 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let N = Number(input[0]);
+let cranes = input[1].split(' ').map(Number).sort((a, b) => b - a);
+let M = Number(input[2]);
+let boxes = input[3].split(' ').map(Number).sort((a, b) => b - a);
+
+if (cranes[0] < boxes[0]) {
+    console.log(-1);
+    return;
+}
+let times = 0;
+let left = M;
+let start = Array(N).fill(0);
+let visited = Array(M).fill(0);
+while (left > 0) {
+  let moved = 0;
+  for (let i = 0; i < N; i++) {
+    for(let j = start[i]; j < M; j++){
+      start[i] = j + 1;
+      if(visited[j])continue;
+      if(cranes[i] >= boxes[j]){
+        visited[j] = 1;
+        left--;
+        moved++;
+        break;
+      }
+    }
+  }
+  times++;
+  if(moved === 0)break;
+}
+
+console.log(times);
