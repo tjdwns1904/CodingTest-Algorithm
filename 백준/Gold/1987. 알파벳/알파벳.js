9fcecaad4bc0f144449ccdf21a4f2911269212bf
tjdwns99 <beans2853@gmail.com>
2026-03-03 17:27:50 +0900

    [Gold IV] Title: 알파벳, Time: 1484 ms, Memory: 11888 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.js
+++ 백준/Gold/1987. 알파벳/알파벳.js
@@ -1,26 +1,20 @@
 let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-let [R, C] = input[0].split(/\s+/).map(Number);
-let board = input.slice(1).map(line => line.split('').map(c => charToNum(c)));
-const moves = [[-1, 0], [1, 0], [0, -1], [0, 1]];
-const n = board[0][0];
+let [R, C] = input[0].split(' ').map(Number);
+let board = input.slice(1).map(line => line.split('').map((c) => c.charCodeAt(0) - 'A'.charCodeAt(0)));
+const dr = [-1, 0, 1, 0];
+const dc = [0, 1, 0, -1];
 let max = 1;
-
-dfs(0, 0, 0 | (1 << n), 1);
-console.log(max);
-
-function charToNum(a){
-  return a.charCodeAt(0) - 64;
-}
-
 function dfs(r, c, visited, cnt){
   if(cnt > max) max = cnt;
-
-  for(const [moveR, moveC] of moves){
-    const [newR, newC] = [r + moveR, c + moveC];
+  for(let i = 0; i < 4; i++){
+    const [newR, newC] = [r + dr[i], c + dc[i]];
     if(newR < 0 || newR >= R || newC < 0 || newC >= C)continue;
     if(visited & (1 << board[newR][newC]))continue;
     dfs(newR, newC, visited | (1 << board[newR][newC]), cnt + 1);
   }
 }
+
+dfs(0, 0, 1 << board[0][0], 1);
+console.log(max);
