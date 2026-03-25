2d0d8a0802eb911eccc72a4f4ac772f573b4865a
tjdwns99 <beans2853@gmail.com>
2026-03-25 21:46:13 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 240 ms, Memory: 26292 KB -BaekjoonHub

---
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.js
@@ -0,0 +1,35 @@
+    const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+let N = Number(input[0]);
+let map = input.slice(1).map(line => line.split(' ').map(Number));
+const dr = [-1, 1, 0, 0];
+const dc = [0, 0, -1, 1];
+let dp = Array.from({ length: N }, () => Array(N).fill(0));
+let max = 1;
+
+for(let i = 0; i < N; i++){
+  for(let j = 0; j < N; j++){
+    let val = dfs(i, j);
+    if(val > max) max = val; 
+  }
+}
+
+function dfs(r, c){
+  if(dp[r][c] !== 0) return dp[r][c];
+  dp[r][c] = 1;
+  for(let i = 0; i < 4; i++){
+    const [nr, nc] = [r + dr[i], c + dc[i]];
+    if(nr < 0 || nr >= N || nc < 0 || nc >= N)continue;
+    if(map[r][c] >= map[nr][nc])continue;
+    dp[r][c] = Math.max(dp[r][c], dfs(nr, nc) + 1);
+  }
+
+  return dp[r][c];
+}
+
+dfs(0, 0);
+console.log(max);
