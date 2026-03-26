921be7343e7ea7d71d37c1a92063507cdca53173
tjdwns99 <beans2853@gmail.com>
2026-03-26 13:12:40 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 244 ms, Memory: 26416 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.js
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.js
@@ -1,4 +1,4 @@
-    const input = require("fs")
+const input = require("fs")
   .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
   .toString()
   .trim()
@@ -8,18 +8,11 @@ let N = Number(input[0]);
 let map = input.slice(1).map(line => line.split(' ').map(Number));
 const dr = [-1, 1, 0, 0];
 const dc = [0, 0, -1, 1];
-let dp = Array.from({ length: N }, () => Array(N).fill(0));
+let dp = Array.from({length: N}, () => Array(N).fill(0));
 let max = 1;
 
-for(let i = 0; i < N; i++){
-  for(let j = 0; j < N; j++){
-    let val = dfs(i, j);
-    if(val > max) max = val; 
-  }
-}
-
 function dfs(r, c){
-  if(dp[r][c] !== 0) return dp[r][c];
+  if(dp[r][c]) return dp[r][c];
   dp[r][c] = 1;
   for(let i = 0; i < 4; i++){
     const [nr, nc] = [r + dr[i], c + dc[i]];
@@ -31,5 +24,10 @@ function dfs(r, c){
   return dp[r][c];
 }
 
-dfs(0, 0);
+for(let i = 0; i < N; i++){
+  for(let j = 0; j < N; j++){
+    max = Math.max(max, dfs(i, j));
+  }
+}
+
 console.log(max);
