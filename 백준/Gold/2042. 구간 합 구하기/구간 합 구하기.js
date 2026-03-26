f3e90fc49c2a07d1910a4bee8c046acf6bf41cb3
tjdwns99 <beans2853@gmail.com>
2026-03-26 13:41:16 +0900

    [Gold I] Title: 구간 합 구하기, Time: 1480 ms, Memory: 231836 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.js
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.js
@@ -1,44 +1,47 @@
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
 
 let [N, M, K] = input[0].split(' ').map(Number);
-let numbers = [0n, ...input.slice(1, N + 1).map(BigInt)];
 let tree = Array(N + 1).fill(0n);
 let answer = [];
-
-function update(idx, number){
-  let i = idx;
-  while(i <= N){
-    tree[i] += number;
-    i += i & -i;
-  }
-}
-
-function getSum(idx){
-  let i = idx;
-  let sum = 0n;
-  while(i > 0){
-    sum += tree[i];
-    i -= i & -i;
-  }
-
-  return sum;
-}
+let arr = [0n, ...input.slice(1, N + 1).map(BigInt)];
 
 for(let i = 1; i <= N; i++){
-  update(i, numbers[i]);
-}
+  update(i, BigInt(arr[i]));
+};
 
 input.slice(N + 1).forEach((line) => {
-  const [a, b, c] = line.split(' ').map(Number);
-  if(a === 1){
-    const diff = BigInt(c) - numbers[b];
-    update(b, BigInt(diff));
-    numbers[b] = BigInt(c);
-  }else{
-    const sum = getSum(c) - getSum(b - 1);
-    answer.push(sum);    
+  let [a, b, c] = line.split(' ').map(Number);
+  if (a === 1) {
+    c = BigInt(c);
+    let diff = c - arr[b];
+    update(b, diff);
+    arr[b] = c;
+  } else {
+    let result = sum(c) - sum(b - 1);
+    answer.push(result);
   }
-})
+});
 
 console.log(answer.join('\n'));
+
+//펜윅 트리
+function update(idx, v) {
+  while(idx <= N){
+    tree[idx] += v;
+    idx += idx & -idx;
+  }
+}
+
+function sum(idx) {
+  let s = 0n;
+  while(idx > 0){
+    s += tree[idx];
+    idx -= idx & -idx;
+  }
+
+  return s;
+}
